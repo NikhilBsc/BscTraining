380c516289910b2375614834933adbdac180afe6
NikhilBsc <nikhil.thimmaiah@bsc-us.co.in>
2026-06-26 14:48:52 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-data = {"apple": 3, "banana": 7, "orange": 5}
-
-# maximum frequency value
-max_freq = max(data.values())
-print(max_freq)  # 7
-
-# optional: key with maximum frequency
-max_key = max(data, key=data.get)
-print(max_key)   
